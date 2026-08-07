8b515d2f289767118085db764d405077661ce26e
skillbobby <skillbobby@outlook.com>
2026-08-07 12:26:06 -0500

    Enhance theme with Auto-Installing & Auto-Uninstalling
    automatically un-discovers the provider

--- src/FilamentSpiggleThemeServiceProvider.php
+++ src/FilamentSpiggleThemeServiceProvider.php
@@ -4,6 +4,7 @@ namespace Spiggle\FilamentSpiggleTheme;
 
 use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentAsset;
+use Filament\Support\Facades\FilamentColor;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -15,14 +16,30 @@ class FilamentSpiggleThemeServiceProvider extends PackageServiceProvider
     {
         $package
             ->name(static::$name)
-            ->hasViews()
             ->hasAssets();
     }
 
     public function packageBooted(): void
     {
+        // 1. Inject the CSS globally to all Filament panels
         FilamentAsset::register([
             Css::make('spiggle-theme', __DIR__ . '/../resources/dist/theme.css'),
         ], 'spiggle/filament-spiggle-theme');
+
+        // 2. Register the TailAdmin primary colors globally
+        FilamentColor::register([
+            'primary' => [
+                50 => '#EEF2FF',
+                100 => '#E0EAFF',
+                200 => '#C7D7FE',
+                300 => '#A4BCFD',
+                400 => '#7A96FC',
+                500 => '#465FFF',
+                600 => '#3544E5',
+                700 => '#2A34B8',
+                800 => '#262D93',
+                900 => '#242B74',
+            ],
+        ]);
     }
 }
